593a2b0a048986317753ab9a70f0fd9bcf2ad3c4
satyasai69 <ch.satya.sai133@gmail.com>
2023-11-04 11:41:08 +0530

    requireserror type

--- error.sol
+++ error.sol
@@ -1,5 +1,8 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 contract error {
-
+    //require(); check the function is true to given condition
+    function requireserror(uint i) public pure {
+        require(i < 10);
+    }
 }
