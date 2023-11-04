e93acc0c6f9cbe49e12cffdf113a9bd0f9cb55a3
satyasai69 <ch.satya.sai133@gmail.com>
2023-11-04 11:45:25 +0530

    revert error type

--- error.sol
+++ error.sol
@@ -5,4 +5,11 @@ contract error {
     function requireserror(uint i) public pure {
         require(i < 10);
     }
+    //its same as requier but it run function and if error like condition no ture its throw error and
+    //refund gas
+    function reverterror(uint _i) public pure {
+        if(_i <=10){
+            revert("Input must be greater than 10");
+        }
+    }
 }
